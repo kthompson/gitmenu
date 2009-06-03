aa4fc5b6faa449ae7895211f3547caa241709ca4
Kevin Thompson <kthompson@pelco.com>
2009-06-03 12:16:13 -0700

    * GitGlyph.cs: Enum to represent custom icon states * Resources/StatusIcons.bmp: Resource by Keith Rocha * StatusIcons.cs: Helper to load an ImageList

---
+++ GitGlyph.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitMenu
+{
+    public enum GitGlyph
+    {
+        None = 0,
+        STATEICON_CHECKEDIN = 1,
+        STATEICON_CHECKEDOUT = 2,
+        STATEICON_ORPHANED = 3,
+        STATEICON_EDITABLE = 4,
+        STATEICON_BLANK = 5,
+        STATEICON_READONLY = 6,
+        STATEICON_DISABLED = 7,
+        STATEICON_CHECKEDOUTEXCLUSIVE = 8,
+        STATEICON_CHECKEDOUTSHAREDOTHER = 9,
+        STATEICON_CHECKEDOUTEXCLUSIVEOTHER = 10,
+        STATEICON_EXCLUDEDFROMSCC = 11,
+        Untracked = 12,
+        Modified = 13,
+        Indexed = 14,
+        Normal = 15,
+    }
+}
---
+++ StatusIcons.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+
+namespace GitMenu
+{
+    public static class StatusIcons
+    {
+        public static ImageList CreateStatusImageList()
+        {
+            using (Stream images = typeof(StatusIcons).Assembly.GetManifestResourceStream("GitMenu.Resources.StatusIcons.bmp"))
+            {
+                if (images == null)
+                    return null;
+
+                Bitmap bitmap = (Bitmap)Image.FromStream(images, true);
+
+                ImageList imageList = new ImageList();
+                imageList.ImageSize = new Size(8, bitmap.Height);
+                bitmap.MakeTransparent(bitmap.GetPixel(0, 0));
+
+                imageList.Images.AddStrip(bitmap);
+
+                return imageList;
+            }
+        }
+    }
+}
